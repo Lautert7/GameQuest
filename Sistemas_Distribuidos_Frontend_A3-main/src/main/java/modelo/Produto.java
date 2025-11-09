8647a6cc0009bc15492aa06ad205d339bd2c03a7
Lautert7 <kaykylautert43@gmail.com>
2025-11-09 12:58:57 -0300

    ImplementaÃ§Ã£o RMI Frontend: Cliente RMI e classes de modelo para comunicaÃ§Ã£o com servidor

---
+++ Sistemas_Distribuidos_Frontend_A3-main/src/main/java/modelo/Produto.java
@@ -0,0 +1,112 @@
+package modelo;
+
+import java.io.Serializable;
+
+public class Produto implements Serializable {
+    private static final long serialVersionUID = 1L;
+    
+    private int id;
+    private String nome;
+    private String unidade;
+    private double preco;
+    private int quantidade;
+    private int min;
+    private int max;
+    private String categoria;
+
+    public Produto() {
+        this(0,"","",0.0,0,0,1000,"");
+    }
+
+    public Produto(int id, String nome, String unidade, double preco, int quantidade,int min, int max, String categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.unidade = unidade;
+        this.preco = preco;
+        this.quantidade = quantidade;
+        this.min = min;
+        this.max = max;
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+    
+    public int getQuantidade(){
+        return quantidade;
+    }
+    
+    public void setQuantidade(int quantidade){
+        this.quantidade = quantidade;  
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String nomeCategoria) {
+        this.categoria = nomeCategoria;
+    }
+    
+    @Override
+    public String toString(){
+        return this.nome;
+    }
+    
+    public String VerificacaoDeQuantidade(){
+        if (this.getQuantidade()<this.getMin()){
+            return "A quantidade do produto: "+getNome()+" /está muito baixa, a quantidade minima é "+getMin()+" unidades";
+            
+        }else if(this.getQuantidade()>this.getMax()){
+            return "A quantidade do produto:"+getNome()+" /é muito alta, a quantidade máxima é "+getMax()+" unidades";
+        }else{
+            return "produto registrado com sucesso. A quantidade é "+getQuantidade()+" unidades";
+        }
+    }
+}
+
